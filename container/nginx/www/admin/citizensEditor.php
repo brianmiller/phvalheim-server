c903efe55a91791f48edfc68c9d07f743e5c3d97
Brian Miller <brian@phospher.com>
2022-10-20 18:13:44 -0700

    citizensEditor.php works!

--- container/nginx/www/admin/citizensEditor.php
+++ container/nginx/www/admin/citizensEditor.php
@@ -1,8 +1,4 @@
 <?php
-
-  
-#include '/opt/stateless/nginx/www/includes/config_env_puller.php';
-#include '/opt/stateless/nginx/www/includes/phvalheim-frontend-config.php';
 include '../includes/db_sets.php';
 include '../includes/db_gets.php';
 
@@ -12,7 +8,9 @@ echo "<br>";
 if (isset($_GET['world']))
 {
 	$world = $_GET['world'];
+        # make the html look prettier replace spaces with new lines... this is just for ease of reading, we don't store carriage returns in the database, see below
 	$currentCitizens = getCitizens($pdo,$world);
+        $currentCitizens = str_replace(' ', PHP_EOL, $currentCitizens);
 	print "World: $world<br>";
 }
 
@@ -21,7 +19,6 @@ if (isset($_GET['citizens'],$_GET['world']))
 {
 	$citizens = $_GET['citizens'];
 	$world = $_GET['world'];
-	print "New Citizens to Write: $citizens<br>";
 
 	# trim and clean up new input, we don't store carriage returns in the database
 	$citizens = str_replace("\r\n", " ", $citizens);
@@ -33,7 +30,6 @@ if (isset($_GET['citizens'],$_GET['world']))
 	# make the html look prettier replace spaces with new lines... this is just for ease of reading, we don't store carriage returns in the database, see above
 	$currentCitizens = str_replace(' ', PHP_EOL, $currentCitizens);
 
-	print strlen($currentCitizens);
 }
 
 ?>
@@ -41,7 +37,7 @@ if (isset($_GET['citizens'],$_GET['world']))
 
 
 <form action='citizensEditor.php'>
-	<textarea cols='30' rows='20' name='citizens'><?php print $currentCitizens;?></textarea>
+	<textarea cols='18' rows='30' name='citizens'><?php print $currentCitizens;?></textarea>
 	<input type='hidden' name='world' value='<?php echo $world;?>'></input>
 	<input type="submit" value="Save" class="submitButton">
 </form>
