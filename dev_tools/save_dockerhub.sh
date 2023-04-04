20f8db3243a1d2d6bd18a3327d50aa3350d6c376
Brian Miller <brian@phospher.com>
2023-04-04 13:51:36 -0700

    dockerhub tag added

--- dev_tools/save_dockerhub.sh
+++ dev_tools/save_dockerhub.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ ! $1 ]; then
+	echo "USAGE: save_dockerhub.sh <tag>"
+	echo "USAGE: save_dockerhub.sh latest"
+	echo "USAGE: save_dockerhub.sh rc"
+	exit 1
+fi
+
 podman login --username=theoriginalbrian docker.io
 podman build --format=docker -t theoriginalbrian/phvalheim-server .
-podman push theoriginalbrian/phvalheim-server:latest
+
+if [ "$1" = "latest" ]; then
+	podman push theoriginalbrian/phvalheim-server:latest
+elif [ "$1" = "rc" ]; then
+	podman push theoriginalbrian/phvalheim-server:rc
+fi
