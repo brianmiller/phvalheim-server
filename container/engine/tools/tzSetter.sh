b7a779f9b82dad0ac04170c88486b016c4c56c92
Brian Miller <brian@phospher.com>
2023-04-17 23:24:03 -0700

    timezone

--- container/engine/tools/tzSetter.sh
+++ container/engine/tools/tzSetter.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-timezone=$(sql "SELECT timezone FROM settings LIMIT 1" 2>/dev/null)
+dbTimezone=$(sql "SELECT timezone FROM settings LIMIT 1" 2>/dev/null)
 
-if [ ! -f /usr/share/zoneinfo/$timezone ]; then
-	echo "`date` [WARN : phvalheim] $timezone is an invalid timezone, setting to UTC..."
-        ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
-        export TZ="Etc/UTC"
+
+# if the database was set to an invalid timezone, set to UTC. Else, set to specified timezone
+if [ ! -f "/usr/share/zoneinfo/$dbTimezone" ]; then
+
+	# if the database is empty
+	if [ -z "$dbTimezone" ]; then
+		dbTimezone="Etc/UTC"
+		echo "`date` [WARN : phvalheim] The database is set to an invalid time zone, setting to UTC..."
+		export TZ="Etc/UTC"
+	else
+		# if the database is not empty but still invalid
+	        echo "`date` [WARN : phvalheim] '$dbTimezone' is an invalid time zone, setting to UTC..."
+		export TZ="Etc/UTC"
+	fi
+
 else
-        ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
-        export TZ="$timezone"
+
+	# if the database is not empty and valid
+	echo "`date` [NOTICE : phvalheim] Setting time zone to '$dbTimezone'..."
+	export TZ="$dbTimezone"
+
 fi
+
+
+# update database
+sql "UPDATE settings SET timezone='$TZ'" 2>/dev/null
+
+# set it
+ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
