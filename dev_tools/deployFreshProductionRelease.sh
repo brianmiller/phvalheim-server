a4279b33e01bde43b9ddfe62e815e010608a5b8c
Brian Miller <brian@phospher.com>
2023-05-16 13:07:50 -0700

    minor changes

--- dev_tools/deployFreshProductionRelease.sh
+++ dev_tools/deployFreshProductionRelease.sh
@@ -14,11 +14,11 @@ podman create \
                -e 'defaultSeed'='szN8qp2lBn' \
                -e 'backupsToKeep'='10' \
                -e 'phvalheimHost'='phvalheim-dev.phospher.com' \
-               -e 'gameDNS'='37648-dev1.phospher.com' \
+               -e 'gameDNS'='37648-dev2.phospher.com' \
                -e 'steamAPIKey'="`cat ../not_git/steamAPIKey.txt`" \
                -e 'phvalheimClientURL'='https://github.com/brianmiller/phvalheim-client/raw/master/published_build/phvalheim-client-installer.exe' \
                -v "/home/brian/docker_persistent/$containerName/":'/opt/stateful':Z \
                -v "/home/brian/docker_persistent/$containerName/backups/":'/opt/stateful/backups':Z \
-	       docker.io/theoriginalbrian/phvalheim-server
+	       docker.io/theoriginalbrian/phvalheim-server:rc
 
 podman start phvalheim-release
